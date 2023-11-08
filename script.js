8b7fb9edc810b691a689b876a14a9e4bf0199708
JudeTheDuud <praiseosai@gmail.com>
2023-11-08 00:24:34 +0000

    Updates
    Added skills section.
    Added contact form.
    Substantial styling updates.

--- script.js
+++ script.js
@@ -3,6 +3,33 @@ const arrow = document.querySelector('#arrow');
 
 arrow.addEventListener('click', () =>{
 window.location.href = "#about";
-}
-    
+} 
 )
+
+
+function toggle (){
+const blur = document.querySelector("#overlay");
+blur.classList.toggle("active");
+
+const contact = document.querySelector("#contact-form");
+contact.classList.toggle("active")
+
+
+
+}
+
+function close(){
+    const close = document.querySelector("#close");
+    const contact = document.querySelector("#contact-form");
+    const blur = document.querySelector("#overlay");
+    close.addEventListener('click', () =>{
+        contact.classList.remove("active");
+        blur.classList.remove("active");
+    })
+
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    close();
+});
